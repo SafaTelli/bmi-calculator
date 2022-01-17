7360a038037c7430454b9cca9a0c4d894e63d279
Safa Telli <safa.telli@gmail.com>
2022-01-17 16:06:19 +0100

    modified imc to bmi

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 var express = require('express');
 const { imc, interpretIMC } = require('./imc');
 var app = express();
-app.get('/imc', function (req, res) {
+app.get('/bmi', function (req, res) {
 
     const weight = req.query.weight;
     const height = req.query.height;
     const ImcRes = imc(weight, height);
-    res.status(200).send({ "imc": ImcRes });
+    res.status(200).send({ "bmi": ImcRes });
 });
 
-app.get('/imc/:n', function (req, res) {
+app.get('/bmi/:n', function (req, res) {
 
     const input = req.params.n;
     console.log(parseFloat(input));
@@ -21,6 +21,3 @@ app.listen(3000, function () {
     console.log('Listening to Port 3000');
 });
 
-app.get('/metrics', function (req, res) {
-    res.status(200).send({ "cpu": "xx%" })
-});
